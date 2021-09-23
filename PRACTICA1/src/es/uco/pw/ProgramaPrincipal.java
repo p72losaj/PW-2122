bd754847b4790698fb172511332daab0f8e70e41
p72losaj <p72losaj@uco.es>
2021-09-23 12:16:42 +0200

    Creacion de la practica1

---
+++ PRACTICA1/src/es/uco/pw/ProgramaPrincipal.java
@@ -0,0 +1,19 @@
+package es.uco.pw;
+
+import java.util.Scanner;
+
+/**
+ * Programa ejecutable 
+ * @author Jaime Lorenzo Sanchez
+ * @version 1.0
+ */
+
+public class ProgramaPrincipal {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner (System.in); // Clase scanner es necesaria para obtener datos por teclado
+		
+	}
+
+}
---
+++ PRACTICA1/src/es/uco/pw/espectador/Espectador.java
@@ -0,0 +1,14 @@
+package es.uco.pw.espectador;
+
+/**
+ * Clase que gestiona la informacion de un espectador
+ * @author Jaime Lorenzo Sanchez
+ *	@version 1.0
+ */
+
+public class Espectador {
+	String nombre; // Nombre del espectador
+	String apellido1; // Primer apellido del espectador
+	String apellido2; // Segundo apellido del espectador
+	
+}
