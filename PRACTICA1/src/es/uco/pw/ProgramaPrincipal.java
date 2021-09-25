f0c9dadc22b17dcb927f9d8794e1d8405532f48b
p72losaj <p72losaj@uco.es>
2021-09-25 11:39:38 +0200

    Creacion del fichero de propiedades

--- PRACTICA1/src/es/uco/pw/ProgramaPrincipal.java
+++ PRACTICA1/src/es/uco/pw/ProgramaPrincipal.java
@@ -1,5 +1,10 @@
 package es.uco.pw;
 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.Scanner;
 
 /**
@@ -10,9 +15,30 @@ import java.util.Scanner;
 
 public class ProgramaPrincipal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
+		
+		Properties prop = new Properties(); // Inicializamos la clase Properties
 		
 		Scanner teclado = new Scanner (System.in); // Clase scanner es necesaria para obtener datos por teclado
+		
+		// Obtenemos la informacion del fichero de propiedades
+		
+		try {
+			// Obtenemos la ubicacion actual del archivo
+			String rutaAbsoluta = new File("").getAbsolutePath();
+			// Obtenemos la ruta del fichero de propiedades
+			String rutaFicheroPropiedades = rutaAbsoluta + "/src/es/uco/pw/ficheros/propiedades.properties";
+			InputStream is = new FileInputStream(rutaFicheroPropiedades);
+			prop.load(is);
+			// Obtenemos la ubicacion del fichero de datos de las criticas
+			String rutaFicheroDatosCriticas = rutaAbsoluta + "/src/es/uco/pw/ficheros/criticas.txt";
+			// Especificamos en el fichero de propiedades la ubicacion del fichero de datos de criticas
+			prop.setProperty("ficheroCriticas", rutaFicheroDatosCriticas);
+			System.out.println(prop.getProperty("ficheroCriticas"));
+		}catch(Exception ex) {
+			System.out.println("Se ha producido un error al obtener la informacion del fichero de propiedades");
+			return;
+		}
 		
 	}
 
