7244bb568bca6f8d1fce1490fffe83970f359dd4
i92exfej <71658639+i92exfej@users.noreply.github.com>
2021-12-30 14:12:06 +0100

    Update ServletCriticas.java

--- PROYECTO_WEB_COPIA/webapp/WEB-INF/classes/servlets/ServletCriticas.java
+++ PROYECTO_WEB_COPIA/webapp/WEB-INF/classes/servlets/ServletCriticas.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/ServletCriticas")
 public class ServletEspectaculo extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 6L;
 
     /**
      * Default constructor. 
